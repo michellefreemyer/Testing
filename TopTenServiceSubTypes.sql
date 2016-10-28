c76295575f52f55819fed6149c0aad9aa255776d
michellefreemyer <michellefreemyer@gmail.com>
2016-10-28 13:30:59 -0500

    CW TopTen Service Sub Types
    sproc to pull top ten service sub types

---
+++ TopTenServiceSubTypes.sql
@@ -0,0 +1,16 @@
+SELECT   a.BusGroup
+		--,a.Board_Name
+		--,ServiceType
+		,ServiceSubType 
+		,CntType = COUNT(ServiceSubType)
+FROM dbo.v_rpt_Service_Summary a
+INNER JOIN dbo.v_rpt_ServiceBoard b
+	ON a.SR_Board_RecID = b.SR_Board_RecID
+WHERE a.Closed_Flag = 0
+	--AND a.Board_Name NOT IN ('XXXXtest board')
+	--AND b.Inactive_Flag = 0
+	AND a.BusGroup = 'App Support'
+	--AND ServiceType='EHR'
+	--AND ServiceSubType='Allergy Module'
+GROUP BY a.BusGroup,/*Board_Name, Servicetype,*/ ServiceSubType
+ORDER BY /*Board_Name,ServiceType,*/ ServiceSubType
